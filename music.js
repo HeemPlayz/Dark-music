18ff91a50d1119502eed1819128978d0964d6edc
Heem <42186537+HeemPlayz@users.noreply.github.com>
2019-06-05 13:56:49 +0400

    Update music.js

--- music.js
+++ music.js
@@ -111,7 +111,8 @@ client.on('message', function(message) {
         message.channel.send('`✔`').then(() => {
             skip_song(message);
             var server = server = servers[message.guild.id];
-            if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+           // if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+		 if (message.guild.voiceConnection) return message.channel.send("**There is no song**")
         });
     }
     else if (message.content.startsWith(prefix + 'vol')) {
@@ -128,7 +129,7 @@ client.on('message', function(message) {
             dispatcher.pause();
         });
     }
-    else if (mess.startsWith(prefix + 'ok')) {
+    else if (mess.startsWith(prefix + 'resume')) {
         if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **__يجب ان تكون في روم صوتي__**');
             message.channel.send('`✔`').then(() => {
             dispatcher.resume();
@@ -140,7 +141,7 @@ client.on('message', function(message) {
         var server = server = servers[message.guild.id];
         if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
     }
-    else if (mess.startsWith(prefix + 'تعال')) {
+    else if (mess.startsWith(prefix + 'join')) {
         if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **__يجب ان تكون في روم صوتي__**');
         message.member.voiceChannel.join().then(message.channel.send(':ok:'));
     }
@@ -227,60 +228,4 @@ function search_video(query, cb) {
 function isYoutube(str) {
     return str.toLowerCase().indexOf('youtube.com') > -1;
 }
- client.on('message', message => {
-     if (message.content === prefix +"help") {
-    const embed = new Discord.RichEmbed()
-     .setColor("RANDOM")
-     .addField(`Zyad,aLmutairi commands:
-
-+about - shows info about the bot
-+ping - checks the bot's latency
-
-  Music:
-
-+play - shows the song that is currently playing
-+play <title|URL|subcommand> - plays the provided song
-+queue [pagenum] - shows the current queue
-+تعال <title|URL|subcommand> - plays the provided song
-+skip - votes to skip the current song
-
-  DJ:
-+ok <title|URL|subcommand> - plays the provided song
-+skip - skips the current song
-+pause - pauses the current song
-+skipt <position> - skips to the specified song
-+stop - stops the current song and clears the queue
-+vol [0-150] - sets or shows volume
-
-For additional help,  `)
-
-      message.channel.send({embed});
-     }
-    });
-	client.on('message', message => {
-    if (message.content.startsWith("habout")) {
-      message.channel.send({
- embed: new Discord.RichEmbed() 
-    .setColor('RED')
-    .addField('**الذاكرة المستخدمة 💾**', `${(process.memoryUsage().rss / 1000000).toFixed()}MB`, true)
-         .addField('**سرعة الاتصال📡**' , `${Date.now() - message.createdTimestamp}` + ' ms')
-        .addField('**وقت الاقلاع⌚**', timeCon(process.uptime()), true)
-        .addField('**استخدام المعالج💿**', `${(process.cpuUsage().rss / 10000).toFixed()}%`, true)
-     })
-    }
-  });
-  client.on('message', message => {
-                                if(!message.channel.guild) return;
-                        if (message.content.startsWith('hping')) {
-                            if(!message.channel.guild) return;
-                            var msg = `${Date.now() - message.createdTimestamp}`
-                            var api = `${Math.round(client.ping)}`
-                            if (message.author.bot) return;
-                        let embed = new Discord.RichEmbed()
-                        .setAuthor(message.author.username,message.author.avatarURL)
-                        .setColor('RANDOM')
-                        .addField('**Time Taken:**',msg + " ms 📶 ")
-                        .addField('**WebSocket:**',api + " ms 📶 ")
-         message.channel.send({embed:embed});
-                        }
-                    });
+ 
