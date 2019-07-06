c74be605bd39ed873d92d304a90c999313f83b52
Heem <42186537+HeemPlayz@users.noreply.github.com>
2019-07-06 12:52:00 +0400

    Update music.js

--- music.js
+++ music.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core');//
 const request = require('request');
 const fs = require('fs');
 const getYoutubeID = require('get-youtube-id');
